acc158344a45fb1eda93547c240b37263be4bf93
jaeheoon <aleh101@naver.com>
2024-10-20 21:02:57 +0900

    Update MemberController.java

--- src/main/java/com/member/controller/MemberController.java
+++ src/main/java/com/member/controller/MemberController.java
@@ -64,6 +64,8 @@ public class MemberController {
 		memberDTO.setEmail(email);
 		memberDTO.setPhoneNumber(phoneNumber);
 		memberDTO.setAddress(address);
+		memberDTO.setKakaoCheck("F");
+		memberDTO.setKakaoProfile("http://img1.kakaocdn.net/thumb/R640x640.q70/?fname=http://t1.kakaocdn.net/account_images/default_profile.jpeg");
 		memberService.join(memberDTO);
 	}
 
